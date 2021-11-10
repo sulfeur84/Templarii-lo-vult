63f254eacb577fbb4413b2ba47683e5ae5d52d4e
PLANCQ Valentin <sulfeur84@gmail.com>
2021-11-10 14:50:04 +0100

    Add movment

--- Assets/Geoffrey/scripts/Controller.cs
+++ Assets/Geoffrey/scripts/Controller.cs
@@ -2,15 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.XInput;
 
 public class Controller : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 10f;
+    private float _speed = 2f;
     private Vector3 _move;
-    private Vector3 _rotate;
-    private float _angle;
     private Rigidbody rb;
     Vector3 rawInputMovement;
     void Start()
@@ -19,7 +16,7 @@ public class Controller : MonoBehaviour
     }
 
     
-    void Update()
+    void FixedUpdate()
     {
         var gamepad = Gamepad.current;
         if (gamepad == null)
@@ -27,67 +24,33 @@ public class Controller : MonoBehaviour
             Debug.Log("No controller detected.");
             return;
         }
-
-        _move.x = gamepad.leftStick.x.ReadValue();
-        _move.z = gamepad.leftStick.y.ReadValue();
-        _rotate = gamepad.rightStick.ReadValue();
         
-       
-
-        if (UnityEngine.InputSystem.Gamepad.current is UnityEngine.InputSystem.XInput.XInputController)
+        _move.x = gamepad.leftStick.x.ReadValue();
+        _move.y = 0f;
+        _move.z = gamepad.leftStick.y.ReadValue();
+        
+        if (gamepad.leftStick.left.wasPressedThisFrame  || gamepad.leftStick.right.wasPressedThisFrame || 
+            gamepad.leftStick.down.wasPressedThisFrame|| gamepad.leftStick.up.wasPressedThisFrame)
         {
-            if (gamepad.leftStick.IsActuated())
-            {
-                rb.MovePosition(rb.position + _move * _speed * Time.fixedDeltaTime);
-            }
-            if (gamepad.rightStick.x.IsActuated())
-            {
-                transform.Rotate(0, _rotate.y, 0);
-            }
-            if (gamepad.rightStick.y.IsActuated())
-            {
-                transform.Rotate(0, -_rotate.y, 0);
-            }
-            if (gamepad.xButton.wasPressedThisFrame)
-            {
-                Debug.Log("X Button is pressed.");
-            }
-            if (gamepad.bButton.wasPressedThisFrame)
-            {
-                Debug.Log("B Button is pressed.");
-            }
-            if (gamepad.yButton.wasPressedThisFrame)
-            {
-                Debug.Log("Y Button is pressed.");
-            }
-            if (gamepad.aButton.wasPressedThisFrame)
-            {
-                Debug.Log("A Button is pressed.");
-            }
+            rb.MovePosition(rb.position + _move * _speed * Time.fixedDeltaTime);
         }
         
-        if (UnityEngine.InputSystem.Gamepad.current is UnityEngine.InputSystem.DualShock.DualShockGamepad)
+        if (gamepad.buttonWest.wasPressedThisFrame)
         {
-            if (gamepad.leftStick.IsActuated())
-            {
-                rb.MovePosition(rb.position + _move * _speed * Time.fixedDeltaTime);
-            }
-            if (gamepad.squareButton.wasPressedThisFrame)
-            {
-                Debug.Log("Square Button is pressed.");
-            }
-            if (gamepad.circleButton.wasPressedThisFrame)
-            {
-                Debug.Log("Circle Button is pressed.");
-            }
-            if (gamepad.triangleButton.wasPressedThisFrame)
-            {
-                Debug.Log("Triangle Button is pressed.");
-            }
-            if (gamepad.crossButton.wasPressedThisFrame)
-            {
-                Debug.Log("Cross Button is pressed.");
-            }
+            
+        }
+        if (gamepad.buttonEast.wasPressedThisFrame)
+        {
+            
+        }
+        if (gamepad.buttonNorth.wasPressedThisFrame)
+        {
+            
+        }
+        if (gamepad.buttonSouth.wasPressedThisFrame)
+        {
+            
         }
     }
+    
 }
